df74b65a5c8d4282299e3d97cfc0067a7478ec61
Paradorn2018 <paradorn_boonpoor@hotmail.com>
2026-01-08 15:22:59 +0700

    Pytest Object

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:forester77@127.0.0.1:3306/todosapplicationdatabase'
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
